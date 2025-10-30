1c2e36c785863630fce514ba46f4b932070e4a67
keithtyser <keithtyser@gmail.com>
2025-10-30 00:43:33 -0400

    letsencrypt update

--- infra/init-letsencrypt.sh
+++ infra/init-letsencrypt.sh
@@ -70,7 +70,8 @@ docker compose run --rm --entrypoint "\
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
-    --force-renewal" certbot
+    --force-renewal \
+    --break-my-certs" certbot
 echo
 
 echo "### Reloading nginx ..."
